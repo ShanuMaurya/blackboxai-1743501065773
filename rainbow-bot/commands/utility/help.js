455a915d17a6665258cb00a6c02b653491daa66c
Patetochips <36756132+ShanuMaurya@users.noreply.github.com>
2025-04-01 18:52:59 +0530

    Add rainbow-bot/commands/utility/help.js 1743513781933

--- rainbow-bot/commands/utility/help.js
+++ rainbow-bot/commands/utility/help.js
@@ -19,6 +19,14 @@ module.exports = {
           value: '`/kick` - Kick a member\n`/ban` - Ban a member\n`/mute` - Timeout a member\n`/warn` - Warn a member\n`/purge` - Delete messages'
         },
         {
+          name: '💞 Roleplay Commands (5)',
+          value: '`/hug` - Hug someone\n`/pat` - Pat someone\n`/kiss` - Kiss someone\n`/cuddle` - Cuddle someone\n`/slap` - Playful slap'
+        },
+        {
+          name: '🔊 VC Commands (10)',
+          value: '`/vc-disconnect` - Disconnect user\n`/vc-mute` - Mute user\n`/vc-unmute` - Unmute user\n`/vc-deafen` - Deafen user\n`/vc-undeafen` - Undeafen user\n`/vc-disconnectall` - Disconnect all\n`/vc-muteall` - Mute all\n`/vc-unmuteall` - Unmute all\n`/vc-deafenall` - Deafen all\n`/vc-undeafenall` - Undeafen all\n`/vc-moveall` - Move all'
+        },
+        {
           name: '🛠️ Utility Commands (5)',
           value: '`/help` - Shows this menu\n`/ping` - Checks bot latency\n`/userinfo` - User information\n`/serverinfo` - Server information\n`/avatar` - Get user avatar\n`/botinfo` - Bot information'
         }
