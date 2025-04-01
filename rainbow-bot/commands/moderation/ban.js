3ce38eaef0f284a9c0b278976d4cbfad82708c40
Patetochips <36756132+ShanuMaurya@users.noreply.github.com>
2025-04-01 16:52:14 +0530

    Add rainbow-bot/commands/moderation/ban.js 1743506537016

---
+++ rainbow-bot/commands/moderation/ban.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('ban')
+    .setDescription('Bans a member from the server')
+    .addUserOption(option =>
+      option.setName('user')
+        .setDescription('The user to ban')
+        .setRequired(true))
+    .addStringOption(option =>
+      option.setName('reason')
+        .setDescription('Reason for banning'))
+    .addIntegerOption(option =>
+      option.setName('days')
+        .setDescription('Days of messages to delete (0-7)')
+        .setMinValue(0)
+        .setMaxValue(7))
+    .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
+  async execute(interaction) {
+    const user = interaction.options.getUser('user');
+    const reason = interaction.options.getString('reason') || 'No reason provided';
+    const days = interaction.options.getInteger('days') || 0;
+
+    await interaction.guild.members.ban(user, { 
+      reason,
+      deleteMessageDays: days 
+    });
+    return interaction.reply(`✅ Banned ${user.tag} (Reason: ${reason})`);
+  }
+};
